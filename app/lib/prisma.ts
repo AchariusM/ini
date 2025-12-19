f67cea342a2c8275810a254cd5e8057e81f637f7
AchariusM <AchariusM@gmail.com>
2025-12-19 15:46:36 +0700

    connect vercel to railway mysql

--- app/lib/prisma.ts
+++ app/lib/prisma.ts
@@ -1,5 +1,4 @@
 import { PrismaClient } from "@prisma/client";
-import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
@@ -8,7 +7,7 @@ const globalForPrisma = globalThis as unknown as {
 export const prisma =
   globalForPrisma.prisma ??
   new PrismaClient({
-    datasourceUrl: process.env.DATABASE_URL,
+    datasourceUrl: process.env.DATABASE_URL!,
   });
 
 if (process.env.NODE_ENV !== "production") {
